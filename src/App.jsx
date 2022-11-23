494f2d121cce13fdc9165737d310fe01a118775d
svdunaev <54572104+svdunaev@users.noreply.github.com>
2022-11-23 19:52:57 +0500

    add main page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { Controls } from './components/Controls';
+import { ALL_COUNTRIES } from './confit';
+import { List } from './components/List';
+import { Card } from './components/Card';
 
 function App() {
+  const [countries, setCountires] = useState([]);
+
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({data}) => setCountires(data))
+  }, []);
+
   return (
     <>
     <Header></Header>
     <Main>
       <Controls />
+      <List>
+        {
+          countries.map(c => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: 'Region',
+                  description: c.region,
+                },
+                {
+                  title: 'Capital',
+                  description: c.capital,
+                },
+              ],
+            };
+            return (
+              <Card key={c.name} {...countryInfo} />
+            )
+          })
+        }
+      </List>
     </Main>
     </>
   );
